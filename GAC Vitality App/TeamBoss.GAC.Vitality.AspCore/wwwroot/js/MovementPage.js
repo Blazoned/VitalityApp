5657c258a810759863fbdf149dad0eb365674fad
Mart <m.vannijnatten@student.fontys.nl>
2018-10-08 13:40:37 +0200

    UI Movement Homescreen

---
+++ GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/wwwroot/js/MovementPage.js
@@ -0,0 +1,17 @@
+﻿var objects =
+    [
+        {
+            private: true,
+            title: "45x Opdrukken",
+            description: "asdkhaskjbdhjksaukdasjkhdgsjhfjsgakudgjgskdgajgdkuga"
+        },
+        {
+            private: false,
+            title: "1km joggen",
+            description: "asdkhaasdsadsafydgsyafdyuasydgyuasyfdskjbdhjksaukdasjkhdgsjhfjsgakudgjgskdgajgdkuga"
+        }
+    ];
+
+$(document).ready(function (e) {
+    $("#challenge-listing").append();
+});
