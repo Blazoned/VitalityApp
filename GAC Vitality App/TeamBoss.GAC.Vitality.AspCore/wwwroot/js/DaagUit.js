d80841fd57f3231e94a2aea7c0e8f4f318736500
Martijn van den Berk <thatverysadcat@gmail.com>
2018-10-09 12:51:29 +0200

    - Toegevoegd DaagUit scherm.

---
+++ GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/wwwroot/js/DaagUit.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $('#focus').ready(function () {
+        var isSelected = false;
+        $(this).click(function () {
+            isSelected = !isSelected;
+            if (isSelected) {
+                $(this).css('background-color', 'red');
+            }
+            else {
+                $(this).css('background-color', 'green');
+            }
+        });
+    });
+});
