1a23a80a68f34273402d5ad5283571149d1b0961
Blazoned <bryankroon@ziggo.nl>
2018-10-10 23:32:25 +0200

    Default Path Set To Movement Controller For Demo

--- GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/Startup.cs
+++ GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/Startup.cs
@@ -43,7 +43,7 @@ namespace TeamBoss.GAC.Vitality.AspCore
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Movement}/{action=Index}/{id?}");
             });
         }
     }
