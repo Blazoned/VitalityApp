ee7cc5b44aa2ab432afa391f04f51f818b819617
Martijn van den Berk <thatverysadcat@gmail.com>
2018-10-22 15:19:38 +0200

    - Changed the _Layout file to include the latest version of Font Awesome - Added Font Awesome icons to the Index and Settings files belonging to the Head system - Changed the head page .css file accordingly for the new FA icons - Changed the settings page javascript to change the FA icon when pressing a button

--- GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/wwwroot/js/settings.js
+++ GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/wwwroot/js/settings.js
@@ -24,14 +24,19 @@
 
 $(".form-control-settings").click(function () {
     var buttonObj = $(this);
-    if (buttonObj.data("public-listing")) {
+    var faIconObj = $(this).find(".fa-settings");
+    if (buttonObj.data("public-listing")) { // The button is set to true, so make it false
         buttonObj.removeClass("btn-success");
+        faIconObj.removeClass("fa-check");
         buttonObj.addClass("btn-danger");
+        faIconObj.addClass("fa-times");
         buttonObj.data("public-listing", false);
     }
-    else {
+    else { // The button is set to false, so make it true
         buttonObj.removeClass("btn-danger");
+        faIconObj.removeClass("fa-times");
         buttonObj.addClass("btn-success");
+        faIconObj.addClass("fa-check");
         buttonObj.data("public-listing", true);
     }
 });
@@ -39,12 +44,12 @@ $(".form-control-settings").click(function () {
 $(document).ready(function () {
     var progressBarArray = $(".progress-bar");
 
-    for (var i = 0, l = progressBarArray.length; i < l; i++) {
+    for (var i = 0, l = progressBarArray.length; i < l; i++) { // Run through all the progress bars and check if they are full
         var actualProgressBar = $(progressBarArray[i]);
         var progressBarWidth = parseInt(actualProgressBar.css("width"), 10);
         var parentWidth = parseInt(actualProgressBar.parent().css("width"), 10);
 
-        if (progressBarWidth >= parentWidth) {
+        if (progressBarWidth >= parentWidth) { // If a progress bar is full, make its background green
             actualProgressBar.css("background-color", "green");
         }
     }
