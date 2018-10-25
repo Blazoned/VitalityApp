8a52614c12aceef7cb1c6b62aa2e2fa0b885e814
Mart <m.vannijnatten@student.fontys.nl>
2018-10-25 13:05:10 +0200

    Foodsystem bezig

---
+++ GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/Controllers/FoodController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamBoss.GAC.Vitality.AspCore.Controllers
+{
+    public class FoodController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateRecept()
+        {
+            return View();
+        }
+
+        public IActionResult SearchRecepts()
+        {
+            return View();
+        }
+
+        public IActionResult ViewRecept()
+        {
+            return View();
+        }
+    }
+}
