e1e7a8417b5ced50a98c8895b7a9229ec8eb00da
Martijn van den Berk <thatverysadcat@gmail.com>
2018-10-25 12:48:53 +0200

    - Added the hidden buttons to the Index file so that the javascript (head-page.js) is able to switch between them. - Added a block-wrapper to the settings to make it appear more in line with the rest of the application. - Changed the navbar in the _Layout file which now sets the navbar title to the set ViewData["Title"] instead of having VitaliteitsApp in front of it.

---
+++ GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/wwwroot/js/head-page.js
@@ -0,0 +1,13 @@
+﻿function doc_keyUp(e) {
+    if (e.ctrlKey && e.keyCode == 32) {
+        var btnVisible = $(".btn-visible");
+        var btnHidden = $(".btn-hidden");
+
+        btnVisible.removeClass("btn-visible");
+        btnVisible.addClass("btn-hidden");
+        
+        btnHidden.removeClass("btn-hidden");
+        btnHidden.addClass("btn-visible");
+    }
+}
+document.addEventListener('keyup', doc_keyUp, false);
