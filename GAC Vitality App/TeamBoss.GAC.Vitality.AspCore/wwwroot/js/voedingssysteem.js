f949c1eb033a03ec05508e1cee0a220a9ad32327
Martijn van den Berk <thatverysadcat@gmail.com>
2018-10-29 12:46:32 +0100

    - Changed the wrappers in the SearchRecepts file. - Added the carousel to the ViewRecept file and its necesary changes.

--- GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/wwwroot/js/voedingssysteem.js
+++ GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/wwwroot/js/voedingssysteem.js
@@ -1,1 +1,11 @@
-﻿
+﻿$(document).ready(function () {
+    changeWidth();
+    window.onresize = function () { changeWidth() };
+});
+
+function changeWidth() {
+    var challengeWrapper = $(".navbar-food");
+    var rowWidth = $(".row").width();
+
+    challengeWrapper.css("width", rowWidth);
+}
