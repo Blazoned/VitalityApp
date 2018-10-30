e14af4799bd866d7331f8fad1e24af1590c2c867
Tom van den Bogaart <bogatom98@gmail.com>
2018-10-30 11:02:05 +0100

    begin gemaakt met jquery

--- GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/wwwroot/js/MovementPage.js
+++ GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/wwwroot/js/MovementPage.js
@@ -12,6 +12,33 @@
         }
     ];
 
-$(document).ready(function (e) {
-    $("#challenge-listing").append();
+$(function () {
+    $.each(objects, function (index, value) {
+        $("#challenge-listing").append("<a href='#' OnClick='DoAction('.value.index','value.title')' class='list-group-item'>" + value.title);       
+    });
+});
+
+
+function DoAction(id, name) {
+    $.ajax({
+        type: "POST",
+        url: "/Movement",
+        data: "id=" + id + "&name=" + name,
+        success: function (msg) {
+            alert("Data Saved: " + msg);
+        }
+    });
+}
+
+
+$(function () {
+    $('.list-group a').click(function(e) {
+        e.preventDefault();
+        $that = $(this);
+        $that.parent().find('a').removeClass('active');
+        $that.addClass('active'); 
+        if ($(this).hasClass("active")) {
+            $("#Challenge-Title").text(value.text);
+        }
+    });
 });
