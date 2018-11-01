e16c39dbf4d37d10aa5da1037b27133ad4373f1e
Tom van den Bogaart <bogatom98@gmail.com>
2018-11-01 10:23:01 +0100

    semi functionality zo goed als af

--- GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/wwwroot/js/MovementPage.js
+++ GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/wwwroot/js/MovementPage.js
@@ -1,34 +1,30 @@
 ﻿var objects =
     [
         {
+            id: 1,
             private: true,
             title: "45x Opdrukken",
-            description: "asdkhaskjbdhjksaukdasjkhdgsjhfjsgakudgjgskdgajgdkuga"
+            description: "Bij deze challenge gaan de gebruikers 45x opdrukken. Het maakt niet uit hoeveel sets je er over doet als je het maar 45x doet.",
+            location: "Laan Ten Rode 5, Eindhoven"
         },
         {
+            id: 2,
             private: false,
-            title: "1km joggen",
-            description: "asdkhaasdsadsafydgsyafdyuasydgyuasyfdskjbdhjksaukdasjkhdgsjhfjsgakudgjgskdgajgdkuga"
+            title: "1KM Hardlopen",
+            description: "Bij deze challenge gaat de gebruiker 1km joggen. Er is geen tijd verbonden aan deze challenge.",
+            location: "Europaweg 45a, Veldhoven"
         }
     ];
 
+
+
+
 $(function () {
-    $.each(objects, function (index, value) {
-        $("#challenge-listing").append("<a href='#' OnClick='DoAction('.value.index','value.title')' class='list-group-item'>" + value.title);       
-    });
+    $.each(objects,
+        function(index, value) {
+            $("#challenge-listing").append("<a href='#' class='list-group-item' data-id='" + value.id + "' data-title = '" + value.title + "' + data-description='" + value.description + "' + data-location='" + value.location + "'>" + value.title + "</a>");
+        });
 });
-
-
-function DoAction(id, name) {
-    $.ajax({
-        type: "POST",
-        url: "/Movement",
-        data: "id=" + id + "&name=" + name,
-        success: function (msg) {
-            alert("Data Saved: " + msg);
-        }
-    });
-}
 
 
 $(function () {
@@ -36,9 +32,10 @@ $(function () {
         e.preventDefault();
         $that = $(this);
         $that.parent().find('a').removeClass('active');
-        $that.addClass('active'); 
-        if ($(this).hasClass("active")) {
-            $("#Challenge-Title").text(value.text);
-        }
+        $that.addClass('active');
+
+        $("#Challenge-Title").text($('.list-group a.active').text());
+        $("#Challenge-Description").text($(this).data("description"));
+        $("#Challenge-Location").text($(this).data("location"));
     });
 });
