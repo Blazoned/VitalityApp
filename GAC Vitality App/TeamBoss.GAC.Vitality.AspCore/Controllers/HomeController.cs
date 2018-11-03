ee1353889faf4015b89f4a73c6c8ff21de7df3d3
Blazoned <bryankroon@ziggo.nl>
2018-11-03 20:51:00 +0100

    Semi-Bug Fixes Merge Surge

--- GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/Controllers/HomeController.cs
+++ GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/Controllers/HomeController.cs
@@ -15,17 +15,13 @@ namespace TeamBoss.GAC.Vitality.AspCore.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Achievements()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult Settings()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
