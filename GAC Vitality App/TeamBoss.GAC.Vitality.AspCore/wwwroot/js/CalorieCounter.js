5d4cd970093b406590e9cb186999474dd51e37f8
Martijn van den Berk <thatverysadcat@gmail.com>
2018-11-29 12:02:38 +0100

    - Added a modal for editing max calories to the CalorieCounter html. - Added a header to the CalorieCounter html.
    - Added a header to the CreateRecept html.
    - Made it so a list of ingredients can be added in CreateRecept html.
    
    - Added a <hr/> to the Index html.
    
    - Added a header to the SavedRecipes html.
    
    - Added a header to the SearchRecepts file.

--- GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/wwwroot/js/CalorieCounter.js
+++ GAC Vitality App/TeamBoss.GAC.Vitality.AspCore/wwwroot/js/CalorieCounter.js
@@ -1,24 +1,23 @@
 ﻿var calorieCounterProgressCircle; // A reference to the calorie counter progress circle object.
-var maxCalorieCount; // The maximum daily amount of calories.
-var maxCaloriePercentage; // The calories to add per percentage.
+var oldMaxCalorieCount = 0;
 
 $(document).ready(function () {
     calorieCounterProgressCircle = $(".progress-circle");
-
-    SetMaxCalorieCount($(calorieCounterProgressCircle).data("max-calories"));
     setInterval(UpdateCalorieCounterCircle, 33);
 });
 
-// Sets the maxCalorieCount to the given value and updates the maxCaloriePercentage variable accordingly.
-function SetMaxCalorieCount(newMaxCalorieCount) {
-    maxCalorieCount = newMaxCalorieCount;
-    maxCaloriePercentage = maxCalorieCount / 100;
-}
-
 function UpdateCalorieCounterCircle() {
+    var maxCalorieCount = $(calorieCounterProgressCircle).attr("data-max-calories");
+
+    if (maxCalorieCount != oldMaxCalorieCount) {
+        oldMaxCalorieCount = maxCalorieCount;
+        $(calorieCounterProgressCircle).attr("data-current-calories", 0);
+        $(calorieCounterProgressCircle).attr("data-progress", 0);
+    }
+
     var currentCalories = $(calorieCounterProgressCircle).attr("data-current-calories"); // The current calories being displayed.
     var targetCalories = $(calorieCounterProgressCircle).attr("data-target-calories"); // The number of calories that should be displayed.
-    
+
     if (currentCalories != targetCalories) {
         var currentCaloriesInt = parseFloat(currentCalories);
         var targetCaloriesInt = parseFloat(targetCalories);
@@ -26,7 +25,9 @@ function UpdateCalorieCounterCircle() {
         var targetPercentageInt = parseInt(targetCaloriesInt / maxCalorieCount * 100); // The percentage that should be displayed.
         var currentPercentage = $(calorieCounterProgressCircle).attr("data-progress"); // The percentage actually being displayed.
         var currentPercentageInt = parseInt(currentPercentage);
-        
+
+        var maxCaloriePercentage = maxCalorieCount / 100;
+
         if (currentPercentageInt < targetPercentageInt) {
             currentPercentageInt += 1;
             currentCaloriesInt += maxCaloriePercentage;
