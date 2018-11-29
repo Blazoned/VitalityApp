872d4843e005970a012daf58f33ece54bb573092
Mart <m.vannijnatten@student.fontys.nl>
2018-11-29 11:03:15 +0100

    Database Connection

---
+++ GAC Vitality App/TeamBoss.GAC.Vitality.Data/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TeamBoss.GAC.Vitality.Data
+{
+    public class Connection
+    {
+        SqlConnection c = new SqlConnection();
+        SqlConnectionStringBuilder b = new SqlConnectionStringBuilder();
+
+        public Connection()
+        {
+            b.UserID = "vitalitydatabase";
+            b.Password = "Eu4q81-5fkU!";
+            b.InitialCatalog = "vitalitydatabase";
+            b.DataSource = "den1.mssql7.gear.host";
+            b.PersistSecurityInfo = true;
+            c.ConnectionString = b.ConnectionString;
+        }
+
+        public string GetConnectionString()
+        {
+            string result = b.ConnectionString;
+            return result;
+        }
+    }
+}
